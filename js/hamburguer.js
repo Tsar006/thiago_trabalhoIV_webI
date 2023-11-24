b4535173d891556780c125dc53fe9376716b27f0
Tsar006 <115905688+Tsar006@users.noreply.github.com>
2023-11-24 10:50:39 -0300

    js e chá

---
+++ js/hamburguer.js
@@ -0,0 +1,9 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    var links = document.getElementById("myLinks");
+    if (links.style.display === "block") {
+        links.style.display = "none";
+    } else {
+        links.style.display = "block";
+    }
+}
